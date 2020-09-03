94820b56caa98db89a47b2c3a97e0ee1faef5bd2
QuantSoldier <quantsoldier@gmail.com>
2020-09-03 13:40:51 -0700

    Update CSP

--- server/server.js
+++ server/server.js
@@ -5,9 +5,22 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 // use helmet
-app.use(helmet({
-  contentSecurityPolicy: false
-}));
+app.use(helmet());
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'"],
+      connectSrc: ["*.rss2json.com"],
+      styleSrcElem: ["*"],
+      fontSrc: ["*"],
+      imgSrc: ["*", "'self'", "data:", "https:"],
+      objectSrc: ["'self'"],
+      upgradeInsecureRequests: [],
+    },
+  })
+);
 
 // serve static assets normally
 app.use(express.static(path.join(__dirname, 'build')));
