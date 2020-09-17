b00ebe1d46b0b93092f060f36bd912cb3bcdc99d
QuantSoldier <quantsoldier@gmail.com>
2020-09-17 09:11:12 -0700

    Add multiple choice lock voting

--- contracts/_Vote.sol
+++ contracts/_Vote.sol
@@ -25,9 +25,173 @@ COPY PASTE THESE
 pragma solidity ^0.6.0;
 
 import "IERC20.sol";
-import "SafeMath.sol";
 //import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/master/contracts/token/ERC20/IERC20.sol";
 //used to give tokens as rewards (tokens need to be loaded on the contract 1st)
+
+library SafeMath{
+    /**
+     * @dev Returns the addition of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `+` operator.
+     *
+     * Requirements:
+     * - Addition cannot overflow.
+     */
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting on
+     * overflow (when the result is negative).
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     * - Subtraction cannot overflow.
+     */
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        return sub(a, b, "SafeMath: subtraction overflow");
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting with custom message on
+     * overflow (when the result is negative).
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     * - Subtraction cannot overflow.
+     *
+     * _Available since v2.4.0._
+     */
+    function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b <= a, errorMessage);
+        uint256 c = a - b;
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the multiplication of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `*` operator.
+     *
+     * Requirements:
+     * - Multiplication cannot overflow.
+     */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+        // benefit is lost if 'b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers. Reverts on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     * - The divisor cannot be zero.
+     */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        return div(a, b, "SafeMath: division by zero");
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers. Reverts with custom message on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     * - The divisor cannot be zero.
+     *
+     * _Available since v2.4.0._
+     */
+    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        // Solidity only automatically asserts when dividing by 0
+        require(b > 0, errorMessage);
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+
+        return c;
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * Reverts when dividing by zero.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     * - The divisor cannot be zero.
+     */
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        return mod(a, b, "SafeMath: modulo by zero");
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * Reverts with custom message when dividing by zero.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     * - The divisor cannot be zero.
+     *
+     * _Available since v2.4.0._
+     */
+    function mod(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b != 0, errorMessage);
+        return a % b;
+    }
+    
+        /**
+     * @dev Returns the largest of two numbers.
+     */
+    function max(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a >= b ? a : b;
+    }
+
+    /**
+     * @dev Returns the smallest of two numbers.
+     */
+    function min(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a < b ? a : b;
+    }
+
+    /**
+     * @dev Returns the average of two numbers. The result is rounded towards
+     * zero.
+     */
+    function average(uint256 a, uint256 b) internal pure returns (uint256) {
+        // (a + b) / 2 can overflow, so we distribute
+        return (a / 2) + (b / 2) + ((a % 2 + b % 2) / 2);
+    }
+}
 
 
 abstract contract Vote {
@@ -38,17 +202,19 @@ abstract contract Vote {
   bytes32 public voteName; // name to describe the vote
   uint256 public voteStart; // UTC timestamp for voteStart
   uint256 public voteEnd; // UTC timestamp for voteEnd
+  bool public decisionActivated; // track whether decision has been activated
 
   uint256 public quorum; // x / 100 = required % of votes / voting power for vote to be actionable
-  uint256 public votesYAY;
-  uint256 public votesNAY;
-  mapping (address => uint256) public votes; //votes (block.time)
+  uint256 public totalVotes; // total votes cast
+  uint256[] public voteChoices; // array of choices to vote for 
+  mapping (address => uint256) public votes; // address => user vote choice
+  mapping (address => uint256) public votingTokens; // address => locked tokens
 
   address public votingPowerToken;
   address public rewardToken;
   uint256 public rewardAmount;
 
-  event voteStart(address _Defiat_Gov, uint256 _voteStart, uint256 _voteEnd, bytes32 _hash, string _voteName);
+  event voteStarting(address _Defiat_Gov, uint256 _voteStart, uint256 _voteEnd, bytes32 _hash, bytes32 _voteName);
 
   modifier OnlyOwner() {
       require(msg.sender == owner);
@@ -61,13 +227,14 @@ abstract contract Vote {
   }
 
   modifier VoteClosed() {
-      require(now > expirationDate, "Vote is still open");
+      require(now > voteEnd, "Vote is still open");
+      require(decisionActivated, "Vote decision must be activated");
       _;
   }
 
   modifier VoteOpen() {
-      require(now > startDate, "Vote is not open");
-      require(now < expirationDate, "Voting has expired");
+      require(now > voteStart, "Vote is not open");
+      require(now < voteEnd, "Voting has expired");
       _;
   }
 
@@ -77,30 +244,16 @@ abstract contract Vote {
   }
 
   modifier QuorumReached {
-    require(votesYAY + votesNAY > IERC20(votingPowerToken).totalSupply() * (quorum / 100), "Not enough votes have been cast");
+    require(totalVotes > IERC20(votingPowerToken).totalSupply() * (quorum / 100), "Not enough votes have been cast");
     _;
   }
 
-  modifier VotePassed {
-      require(votesYAY + votesNAY > IERC20(votingPowerToken).totalSupply() * (quorum / 100), "Not enough votes have been cast");
-      require(votesYAY > votesNAY, "Voted against");
-      require(expirationDate < now, "Voting still ongoing");
-      _;
-  }
-
-
-
-// INITIALIZE
-    /*function setupTokens(address _votingPowerToken, address _rewardToken, uint256 _rewardAmount) external OnlyOwner {
-        rewardToken = _rewardToken;
-        rewardAmount = _rewardAmount;
-        votingPowerToken = _votingPowerToken;
-    }*/
   constructor(
       address _DeFiat_Gov,
       uint256 _delayStartHours,
       uint256 _durationHours, 
-      uint256 _voteName,
+      bytes32 _voteName,
+      uint256 _voteChoices,
       uint256 _quorum,
       address _votingPowerToken, 
       address _rewardToken, 
@@ -108,45 +261,43 @@ abstract contract Vote {
   ) public {
       owner = msg.sender;
       DeFiat_Gov = _DeFiat_Gov;
-      voteStart = block.timestamp + _delayStartHours.mul(3600);
-      voteEnd = voteStart + _durationHours.mul(3600);
+      voteStart = block.timestamp + (_delayStartHours * 3600);
+      voteEnd = voteStart + (_durationHours * 3600);
       voteName = _voteName;
+      voteChoices = new uint256[](_voteChoices);
       rewardToken = _rewardToken;
       rewardAmount = _rewardAmount;
       votingPowerToken = _votingPowerToken;
       quorum = _quorum;
       decisionActivated = false;
 
-      bytes32 _hash = sha256(abi.encodePacked(DeFiat_Gov, expirationDate, _value));
-      emit voteStart(DeFiat_Gov, voteStart, voteEnd,  _hash, voteName);
+      bytes32 _hash = sha256(abi.encodePacked(DeFiat_Gov, voteEnd));
+      emit voteStarting(DeFiat_Gov, voteStart, voteEnd,  _hash, voteName);
   }
 
   // 0 - define virtual proposal action function
   //    all new votes will override this method with the intended function to be activated on vote passing
-  function proposalAction() public virtual VotePassed returns (bool);
+  function proposalAction() public virtual returns (bool);
 
   //1- define ACTIVATION function
-  function activateDecision() external VotePassed { //anybody can activate this.
-      require(expirationDate < now, "Voting still ongoing");
-      require(!decisionActivated, "Vote decision has already been activated");
+  function activateDecision() external { //anybody can activate this.
+        require(voteEnd < now, "Voting still ongoing");
+        require(!decisionActivated, "Vote decision has already been activated");
 
-      proposalAction();
-      
-      voteIsOpen = false; //close vote
-  }
-  
-
-  //2- Gather votes
-  function voteYAY() external VoteOpen CanVote TokenHolder {
-      votes[msg.sender] = block.number; //registers voteNAY
-      votesYAY = votesYAY + myVotingPower(); //we can use external
-      _sendReward(msg.sender);
+        decisionActivated = true; // mark decision activated
+        proposalAction();
   }
 
-  function voteNAY() external VoteOpen CanVote TokenHolder {
-      votes[msg.sender] = block.number; //registers voteNAY
-      votesNAY = votesNAY + myVotingPower();
-      _sendReward(msg.sender);
+  function vote(uint voteChoice, uint256 votePower) external VoteOpen CanVote TokenHolder {
+        require(voteChoice < voteChoices.length, "Invalid vote choice");
+        
+        IERC20(votingPowerToken).transferFrom(msg.sender, address(this), votePower); // transfer tokens to contract
+        votes[msg.sender] = voteChoice; // log of user vote 
+        votingTokens[msg.sender] = votingTokens[msg.sender] + votePower; // increase locked token pointer
+        voteChoices[voteChoice] = voteChoices[voteChoice] + votePower; // increase vote count
+        totalVotes = totalVotes + votePower; // increase total votes
+
+        _sendReward(msg.sender);
   }
 
   //3- reward voters
@@ -155,10 +306,24 @@ abstract contract Vote {
         IERC20(rewardToken).transfer(_address, rewardAmount);
       }
   } //rewards if enough in the pool
+
+  //4- claim tokens when vote is over
+  function claimTokens() external VoteClosed {
+        require(votingTokens[msg.sender] > 0, "No tokens to claim");
+
+        uint256 votingPower = votingTokens[msg.sender];
+        IERC20(votingPowerToken).transfer(msg.sender, votingPower);
+        votingTokens[msg.sender] = votingTokens[msg.sender] - votingPower;
+
+  } 
     
   //0- Misc functions
-  function forceVoteStatus(bool _open) external OnlyOwner {
-      voteIsOpen = _open;
+  function forceVoteEnd() external OnlyOwner {
+      voteEnd = now;
+  }
+
+  function forceDecision(bool _decision) external OnlyOwner {
+      decisionActivated = _decision;
   }
   
   function killContract() external OnlyOwner {
---
+++ contracts/proposals/FeeBurnRateVote.sol
@@ -0,0 +1,54 @@
+pragma solidity ^0.6.0;
+
+import "../_Vote.sol";
+
+contract FeeBurnRateVote is Vote {
+  constructor (
+    address _DeFiat_Gov,
+    uint256 _delayStartHours,
+    uint256 _durationHours, 
+    bytes32 _voteName,
+    uint256 _voteChoices,
+    uint256 _quorum,
+    address _votingPowerToken, 
+    address _rewardToken, 
+    uint256 _rewardAmount
+  ) public 
+    Vote(
+      _DeFiat_Gov, 
+      _delayStartHours,
+      _durationHours, 
+      _voteName,
+      _voteChoices,
+      _quorum,
+      _votingPowerToken, 
+      _rewardToken, 
+      _rewardAmount)
+  {
+  }
+
+  function proposalAction() public override QuorumReached returns (bool) {
+    uint256 newRate;
+    uint256 winningChoice;
+    uint256 maxVotes = 0;
+    for (uint i = 0; i < voteChoices.length; i++) {
+      if (voteChoices[i] >= maxVotes) {
+        winningChoice = i;
+        maxVotes = voteChoices[i];
+      }
+    }
+
+    if (winningChoice == 0) {
+      newRate = 0;
+    } else if (winningChoice == 1) {
+      newRate = 10;
+    } else {
+      newRate = 50;
+    }
+
+    IDeFiat_Gov(DeFiat_Gov).changeFeeRate(newRate);
+    IDeFiat_Gov(DeFiat_Gov).changeBurnRate(newRate);
+
+    return true;
+  }
+}
