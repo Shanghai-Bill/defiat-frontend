e81226cdf9eab0ef3783f6fd7f156eda863c15c9
masterminddefiat <69473319+masterminddefiat@users.noreply.github.com>
2020-09-24 08:31:24 -0600

    Update DeFiat_EXTFarming.sol
    Code cleaning and minor updates.

--- contracts/DeFiat_EXTFarming.sol
+++ contracts/DeFiat_EXTFarming.sol
@@ -229,82 +229,6 @@ contract Context {
     }
 }
 
-// File: @openzeppelin/contracts/ownership/Ownable.sol
-/**
- * @dev Contract module which provides a basic access control mechanism, where
- * there is an account (an owner) that can be granted exclusive access to
- * specific functions.
- *
- * This module is used through inheritance. It will make available the modifier
- * `onlyOwner`, which can be applied to your functions to restrict their use to
- * the owner.
- */
-contract Ownable is Context {
-    address private _owner;
-
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-    /**
-     * @dev Initializes the contract setting the deployer as the initial owner.
-     */
-    constructor () internal {
-        _owner = _msgSender();
-        emit OwnershipTransferred(address(0), _owner);
-    }
-
-    /**
-     * @dev Returns the address of the current owner.
-     */
-    function owner() public view returns (address) {
-        return _owner;
-    }
-
-    /**
-     * @dev Throws if called by any account other than the owner.
-     */
-    modifier onlyOwner() {
-        require(isOwner(), "Ownable: caller is not the owner");
-        _;
-    }
-
-
-    /**
-     * @dev Returns true if the caller is the current owner.
-     */
-    function isOwner() public view returns (bool) {
-        return _msgSender() == _owner;
-    }
-
-    /**
-     * @dev Leaves the contract without owner. It will not be possible to call
-     * `onlyOwner` functions anymore. Can only be called by the current owner.
-     *
-     * NOTE: Renouncing ownership will leave the contract without an owner,
-     * thereby removing any functionality that is only available to the owner.
-     */
-    function renounceOwnership() public onlyOwner {
-        emit OwnershipTransferred(_owner, address(0));
-        _owner = address(0);
-    }
-
-    /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     * Can only be called by the current owner.
-     */
-    function transferOwnership(address newOwner) public onlyOwner {
-        _transferOwnership(newOwner);
-    }
-
-    /**
-     * @dev Transfers ownership of the contract to a new account (`newOwner`).
-     */
-    function _transferOwnership(address newOwner) internal {
-        require(newOwner != address(0), "Ownable: new owner is the zero address");
-        emit OwnershipTransferred(_owner, newOwner);
-        _owner = newOwner;
-    }
-} //adding ALLOWED method
-
 // File: @openzeppelin/contracts/token/ERC20/IERC20.sol
 /**
  * @dev Interface of the ERC20 standard as defined in the EIP. Does not include
@@ -321,133 +245,62 @@ interface IERC20 {
     event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
-// File: @openzeppelin/contracts/utils/Address.sol
-/**
- * @dev Collection of functions related to the address type
- */
-library Address {
-    /**
-     * @dev Returns true if `account` is a contract.
-     *
-     * This test is non-exhaustive, and there may be false-negatives: during the
-     * execution of a contract's constructor, its address will be reported as
-     * not containing a contract.
-     *
-     * IMPORTANT: It is unsafe to assume that an address for which this
-     * function returns false is an externally-owned account (EOA) and not a
-     * contract.
-     */
-    function isContract(address account) internal view returns (bool) {
-        // This method relies in extcodesize, which returns 0 for contracts in
-        // construction, since the code is only stored at the end of the
-        // constructor execution.
-
-        // According to EIP-1052, 0x0 is the value returned for not-yet created accounts
-        // and 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470 is returned
-        // for accounts without code, i.e. `keccak256('')`
-        bytes32 codehash;
-        bytes32 accountHash = 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470;
-        // solhint-disable-next-line no-inline-assembly
-        assembly { codehash := extcodehash(account) }
-        return (codehash != 0x0 && codehash != accountHash);
-    }
-
-    /**
-     * @dev Converts an `address` into `address payable`. Note that this is
-     * simply a type cast: the actual underlying value is not changed.
-     *
-     * _Available since v2.4.0._
-     */
-    function toPayable(address account) internal pure returns (address payable) {
-        return address(uint160(account));
-    }
-
-    /**
-     * @dev Replacement for Solidity's `transfer`: sends `amount` wei to
-     * `recipient`, forwarding all available gas and reverting on errors.
-     *
-     * https://eips.ethereum.org/EIPS/eip-1884[EIP1884] increases the gas cost
-     * of certain opcodes, possibly making contracts go over the 2300 gas limit
-     * imposed by `transfer`, making them unable to receive funds via
-     * `transfer`. {sendValue} removes this limitation.
-     *
-     * https://diligence.consensys.net/posts/2019/09/stop-using-soliditys-transfer-now/[Learn more].
-     *
-     * IMPORTANT: because control is transferred to `recipient`, care must be
-     * taken to not create reentrancy vulnerabilities. Consider using
-     * {ReentrancyGuard} or the
-     * https://solidity.readthedocs.io/en/v0.5.11/security-considerations.html#use-the-checks-effects-interactions-pattern[checks-effects-interactions pattern].
-     *
-     * _Available since v2.4.0._
-     */
-    function sendValue(address payable recipient, uint256 amount) internal {
-        require(address(this).balance >= amount, "Address: insufficient balance");
-
-        // solhint-disable-next-line avoid-call-value
-        (bool success, ) = recipient.call.value(amount)("");
-        require(success, "Address: unable to send value, recipient may have reverted");
-    }
-}
-
-
-/**
- * @dev getter for the amount of DFT staked in the Dungeon
- */
 interface Idungeon {
     function myStake(address _address) external view returns(uint256);
-}
+    }
 
-// File: @defiat-crypto/defiat/blob/master/contracts/DeFiat_FarmingEXT.sol
+
+// File: @defiat-crypto/defiat/blob/master/contracts/XXXXXX.sol
 /**
  * @dev Delegated Farming Contract.
- * Solution for 3rd party tokens that want to use our Staking contract.
- * Project owners get poolOperator rights to manage the staking yield and conditions.
- *
- * FarmingEXT implements a condition on the DFT-DFT farming pool ("dungeon" 
- * Staking the DFT token in the Dungeon boost the yielding rate of the users
- * generating up to 200% rewards (when 100 or more DFT are staked)
+ * Implements a conditoin on the DFT-DFT farming pool for users to generate more rewards
  */
-contract DeFiat_FarmingEXT_V1 {
+contract DeFiat_EXTFarming_V2 {
     using SafeMath for uint256;
 
     //Structs
     struct PoolMetrics {
         address stakedToken;
-        uint256 staked;             // sum of tokens staked in the contract
-        uint256 stakingFee;         // entry fee
+        uint256 staked;         // sum of tokens staked in the contract
+        uint256 stakingFee;     // entry fee
         
         uint256 stakingPoints;
 
         address rewardToken;
         uint256 rewards;        // current rewards in the pool
+        uint256 totalRewards;
 
         uint256 startTime;      // when the pool opens
         uint256 closingTime;    // when the pool closes. 
         uint256 duration;       // duration of the staking
-        uint256 lastEvent;   // last time metrics were updated.
+        uint256 lastEvent;      // last time metrics were updated.
         
-        uint256  ratePerToken;      // CALCULATED pool reward Rate per Token (calculated based on total stake and time)
+        uint256  ratePerToken;  // CALCULATED pool reward Rate per Token (calculated based on total stake and time)
         
         address DftDungeon;     // used to calculate the DeFiatScore
+        bool boostedRewards;
     }
     PoolMetrics public poolMetrics;
 
     struct UserMetrics {
-            uint256 stake;          // native token stake (balanceOf)
-            uint256 stakingPoints;  // staking points at lastEvent
-            uint256 poolPoints;     // pool point at lastEvent
-            uint256 lastEvent;
+        uint256 stake;          // native token stake (balanceOf)
+        uint256 stakingPoints;  // staking points at lastEvent
+        uint256 poolPoints;     // pool point at lastEvent
+        uint256 lastEvent;
 
-            uint256 rewardAccrued;  // accrued rewards over time based on staking points
-            uint256 rewardsPaid;    // for information only
+        uint256 rewardAccrued;  // accrued rewards over time based on staking points
+        uint256 rewardsPaid;    // for information only
 
-            uint256 lastTxBlock;    // latest transaction from the user (antiSpam)
-            uint256 deFiatScore;    // amount of DFT on the DFT-DFT contract
+        uint256 lastTxBlock;    // latest transaction from the user (antiSpam)
     }
     mapping(address => UserMetrics) public userMetrics;
+    
+        address public poolOperator; address public owner;
+        
 
 //== constructor 
     constructor(address _stakedToken, address _rewardToken, uint256 _feeBase1000, uint256 _durationHours, uint256 _delayStartHours) public {
+        owner = msg.sender;
         poolOperator = msg.sender;
         
         poolMetrics.stakedToken = address(_stakedToken);
@@ -459,7 +312,7 @@ contract DeFiat_FarmingEXT_V1 {
         poolMetrics.closingTime = block.timestamp + poolMetrics.duration;
         
         poolMetrics.stakingPoints = 1; //avoirds div by 0 at start
-        FullRewards = true;
+        poolMetrics.boostedRewards = true;
     }
 
 //==Events
@@ -482,19 +335,39 @@ contract DeFiat_FarmingEXT_V1 {
         require(block.timestamp > poolMetrics.closingTime,"Pool not ended Yet"); //good for delayed starts.
         _;
     }
+    
     modifier antiSpam(uint256 _blocks) {
         require(block.number > userMetrics[msg.sender].lastTxBlock.add(_blocks), "Wait X BLOCKS between Transactions");
         userMetrics[msg.sender].lastTxBlock = block.number; //update
         _;
     } 
+    modifier onlyPoolOperator() {
+        require(msg.sender== poolOperator || msg.sender == owner, "msg.sender is not allowed to operate Pool");
+        _;
+    }
+    modifier onlyOwner() {
+        require(msg.sender== owner, "Only Owner");
+        _;
+    }
+    modifier antiWhale(address _address) {
+        require(myStakeShare(_address) < 20000, "User stake% share too high. Leave some for the smaller guys ;-)"); //max 20%
+        _;
+    } 
+    // avoids stakes being deposited once a user reached 20%. 
+    // Simplistic implementation as if we calculate "futureStake" value very 1st stakers will not be able to deposit.
+    
     
 //==Basics 
     function currentTime() public view returns (uint256) {
         return SafeMath.min(block.timestamp, poolMetrics.closingTime); //allows expiration
     } // SafeMath.min(now, endTime)
+    function setPoolOperator(address _address) public onlyPoolOperator {
+        poolOperator = _address;
+    }
+    
     
 //==DeFiat Boost
-    function setDungeon(address _dungeon) public onlyPoolOperator {
+    function setDungeon(address _dungeon) public onlyOwner {
         poolMetrics.DftDungeon = _dungeon;
     }
     
@@ -555,12 +428,13 @@ contract DeFiat_FarmingEXT_V1 {
         return true;
     }
     
+    
 //==Rewards
     function viewTrancheReward(uint256 _period) internal view returns(uint256) {
         //uint256 _poolRewards = poolMetrics.rewards; //tokens in the pool. Note: This can be setup to a fixed amount (totalRewards)
-        uint256 _poolRewards = totalRewards; 
+        uint256 _poolRewards = poolMetrics.totalRewards; 
         
-        if(FullRewards == false){ _poolRewards = SafeMath.min(poolMetrics.staked, _poolRewards);} 
+        if(poolMetrics.boostedRewards == false){ _poolRewards = SafeMath.min(poolMetrics.staked, _poolRewards);} 
         // baseline is the min( staked, rewards); avoids ultra_farming > staking pool - EXPERIMENTAL
         
         uint256 _timeRate = _period.mul(1e18).div(poolMetrics.duration);
@@ -624,13 +498,6 @@ contract DeFiat_FarmingEXT_V1 {
     
 //==staking & unstaking
 
-    modifier antiWhale(address _address) {
-        require(myStakeShare(_address) < 20000, "User stake% share too high. Leave some for the smaller guys ;-)"); //max 20%
-        _;
-    } 
-    // avoids stakes being deposited once a user reached 20%. 
-    // Simplistic implementation as if we calculate "futureStake" value very 1st stakers will not be able to deposit.
-    
     function stake(uint256 _amount) public poolLive antiSpam(1) antiWhale(msg.sender){
         require(_amount > 0, "Cannot stake 0");
         
@@ -692,24 +559,12 @@ contract DeFiat_FarmingEXT_V1 {
         if(block.timestamp <= poolMetrics.startTime || poolMetrics.rewards == 0){return 0;}
         else { return userMetrics[_address].rewardAccrued.add(viewAdditionalRewardOf(_address));} //previousLock + time based extra
     }
+ 
 
 //== OPERATOR FUNCTIONS ==
-
-    address public poolOperator;
     
-    function setPoolOperator(address _address) public onlyPoolOperator {
-        poolOperator = _address;
-    }
-    modifier onlyPoolOperator() {
-        require(msg.sender == poolOperator, "msg.sender is not allowed to operate Pool");
-        _;
-    }
-    
-    bool public FullRewards;
-    uint256 totalRewards;
-    
-    function setFullRewards(bool _bool) public onlyPoolOperator {
-        FullRewards = _bool;
+    function setBoostedRewards(bool _bool) public onlyPoolOperator {
+        poolMetrics.boostedRewards = _bool;
     }
     function loadRewards(uint256 _amount, uint256 _preStake) public onlyPoolOperator { //load tokens in the rewards pool.
         
@@ -722,7 +577,7 @@ contract DeFiat_FarmingEXT_V1 {
         poolMetrics.staked = SafeMath.add(poolMetrics.staked,_preStake);}  // creates baseline for pool. Avoids massive movements on rewards
         
         poolMetrics.rewards = SafeMath.add(poolMetrics.rewards,amount);
-        totalRewards = totalRewards.add(_amount);
+        poolMetrics.totalRewards = poolMetrics.totalRewards.add(_amount);
     }    
     function setFee(uint256 _fee) public onlyPoolOperator {
         poolMetrics.stakingFee = _fee;
@@ -733,7 +588,7 @@ contract DeFiat_FarmingEXT_V1 {
             IERC20(_ERC20address).transfer(_recipient, _amount); //use of the _ERC20 traditional transfer
             //return true;
         } //get tokens sent by error to contract
-    function killPool() public onlyPoolOperator poolEnded returns(bool) {
+    function killPool() public onlyOwner poolEnded returns(bool) {
             selfdestruct(msg.sender);
         } //frees space on the ETH chain
 
